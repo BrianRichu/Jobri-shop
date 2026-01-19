bdecfa8197285d48187c493ad086f309d3691da0
Brian Richu <richubrian427@gmail.com>
2026-01-19 14:22:23 +0300

    created the cart array to store items and made the add to cart button interactive

--- Jobri-Collection/scripts/productsPage.js
+++ Jobri-Collection/scripts/productsPage.js
@@ -1,4 +1,5 @@
 import { products } from "../data/products.js";
+import { cart } from "../data/cart.js";
 
 let productsHTML = "";
 
@@ -8,8 +9,36 @@ products.forEach((product)=>{
           <img src="${product.image}">
           <p class="product-name">${product.name}</p>
           <p class="product-price">ksh ${product.priceShillings}</p>
-          <button class="add-cart-button">Add to Cart</button>
+          <button class="add-cart-button js-add-to-cart" data-product-id = "${product.id}">Add to Cart</button>
         </div>`
 })
 
 document.querySelector('.js-top-products').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    //check first if the item is in the cart
+    cart.forEach((cartItem)=>{
+      if(productId===cartItem.productId){
+        matchingItem = cartItem;
+      }
+    })
+    //if item is in the cart just increase the quantity
+    if(matchingItem){
+      matchingItem.quantity ++;
+    }
+    //if the item is not in the cart create new item object and add it to the cart
+    else{
+      cart.push({
+      productId,
+      quantity : 1
+    })
+    }
+    console.log(cart)
+  })
+})
+
