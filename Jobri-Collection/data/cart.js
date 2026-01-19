cc5d3c08f13a3d8ac608459a354548fb2867991d
Brian Richu <richubrian427@gmail.com>
2026-01-19 16:05:21 +0300

    move add to cart function into cart.js

--- Jobri-Collection/data/cart.js
+++ Jobri-Collection/data/cart.js
@@ -1,3 +1,25 @@
-export const cart = [
+export const cart = [];
 
-];
+export function addToCart(productId) {
+  //checking if the product is already inside the cart
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  //if the item is in the cart increase the quantity only
+  if (matchingItem) {
+    matchingItem.quantity++;
+  }
+
+  //if not in the cart then create the cart item object in the cart
+  else {
+    cart.push({
+      productId,
+      quantity: 1,
+    });
+  }
+}
--- Jobri-Collection/scripts/productsPage.js
+++ Jobri-Collection/scripts/productsPage.js
@@ -1,53 +1,33 @@
 import { products } from "../data/products.js";
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 
 let productsHTML = "";
 
 products.forEach((product) => {
   productsHTML += `
   <div class="top-product">
-          <img src="${product.image}">
-          <p class="product-name">${product.name}</p>
-          <p class="product-price">ksh ${product.priceShillings}</p>
-          <button class="add-cart-button js-add-to-cart" data-product-id = "${product.id}">Add to Cart</button>
-        </div>`;
+    <img src="${product.image}">
+    <p class="product-name">${product.name}</p>
+    <p class="product-price">ksh ${product.priceShillings}</p>
+    <button class="add-cart-button js-add-to-cart" data-product-id = "${product.id}">Add to Cart</button>
+  </div>`;
 });
 
 document.querySelector(".js-top-products").innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  //updating the cart quantity
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-
-
-    //checking if the product is already inside the cart
-    let matchingItem;
-
-    cart.forEach((cartItem) => {
-      if (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
-    });
-
-    //if the item is in the cart increase the quantity only
-    if (matchingItem) {
-        matchingItem.quantity++;
-      } 
-      
-      //if not in the cart then create the cart item object in the cart
-      else {
-        cart.push({
-          productId,
-          quantity: 1,
-        });
-      }
-      //updating the cart quantity
-      let cartQuantity = 0;
-      cart.forEach((cartItem) =>{
-        cartQuantity += cartItem.quantity;
-      })
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    addToCart(productId);
+    updateCartQuantity();
   });
 });
-
-
