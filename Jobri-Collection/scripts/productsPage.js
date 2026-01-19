274c1fedcff201bd6a7cc57fd902edd9b2831600
Brian Richu <richubrian427@gmail.com>
2026-01-19 15:42:36 +0300

    made add to cart button interactive

--- Jobri-Collection/scripts/productsPage.js
+++ Jobri-Collection/scripts/productsPage.js
@@ -3,42 +3,44 @@ import { cart } from "../data/cart.js";
 
 let productsHTML = "";
 
-products.forEach((product)=>{
+products.forEach((product) => {
   productsHTML += `
   <div class="top-product">
           <img src="${product.image}">
           <p class="product-name">${product.name}</p>
           <p class="product-price">ksh ${product.priceShillings}</p>
           <button class="add-cart-button js-add-to-cart" data-product-id = "${product.id}">Add to Cart</button>
-        </div>`
-})
+        </div>`;
+});
 
-document.querySelector('.js-top-products').innerHTML = productsHTML;
+document.querySelector(".js-top-products").innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-  button.addEventListener('click', ()=>{
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
+
+    //checking if the product is already inside the cart
     let matchingItem;
 
-    //check first if the item is in the cart
-    cart.forEach((cartItem)=>{
-      if(productId===cartItem.productId){
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
         matchingItem = cartItem;
       }
-    })
-    //if item is in the cart just increase the quantity
-    if(matchingItem){
-      matchingItem.quantity ++;
-    }
-    //if the item is not in the cart create new item object and add it to the cart
-    else{
-      cart.push({
-      productId,
-      quantity : 1
-    })
-    }
-    console.log(cart)
-  })
-})
+    });
 
+    //if the item is in the cart increase the quantity only
+    if (matchingItem) {
+        matchingItem.quantity++;
+      } 
+      
+      //if not in the cart then create the cart item object in the cart
+      else {
+        cart.push({
+          productId,
+          quantity: 1,
+        });
+      }
+    console.log(cart)
+  });
+});
